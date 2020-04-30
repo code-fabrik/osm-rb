86e97801c13c60cb8b1b3e5a63e8b8ee97d795b8
Lukas_Skywalker <lukas.diener@hotmail.com>
2020-04-30 15:06:15 +0200

    Add exporter; release 0.0.2

--- lib/osm.rb
+++ lib/osm.rb
@@ -1,5 +1,5 @@
 module OSM
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
 
   require_relative 'osm/calculator'
   require_relative 'osm/exporter'
--- lib/osm/exporter.rb
+++ lib/osm/exporter.rb
@@ -1,7 +1,8 @@
-module OSM
-  TILE_SIZE = 256
+require 'typhoeus'
 
+module OSM
   class Exporter
+    TILE_SIZE = 256
     attr_reader :tile_urls, :zoom
 
     def initialize(tile_urls, zoom)
@@ -9,11 +10,11 @@ module OSM
       @zoom = zoom
     end
 
-    def download_tiles(tiles)
+    def download_tiles_as_blob(tiles)
       url = tile_urls.sample()
       hydra = Typhoeus::Hydra.new
       requests = tiles.map do |tile|
-        tile_url = url.gsub('{x}', tile.x).gsub('{y}', tile.y).gsub('{z}', zoom)
+        tile_url = url.gsub('{x}', tile.x.to_s).gsub('{y}', tile.y.to_s).gsub('{z}', zoom.to_s)
         request = Typhoeus::Request.new(tile_url)
         hydra.queue(request)
         request
